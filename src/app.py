d5e00aefc0b5e88fe5653d46dd45c574c7bfb3ca
Mark Lee <leemark@gmail.com>
2024-02-02 21:19:15 -0700

    working v0.1

---
+++ src/app.py
@@ -0,0 +1,114 @@
+
+import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+def get_vectorstore_from_url(url):
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    # split document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+
+    # create vector store
+    vectorstore = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+
+    return vectorstore
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+    retriever = vector_store.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"), 
+        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation"),
+    ])
+
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return retriever_chain
+
+def get_conversational_rag_chain(retriever_chain):
+    llm=ChatOpenAI()
+    prompt= ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's question based on the following context:\n\n{context}\n\n"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+
+    response = conversation_rag_chain.invoke({
+        "input": user_query,
+        "chat_history": st.session_state.chat_history,
+    })
+    return response['answer']
+
+# app config
+st.set_page_config(page_title="Chat w/web", page_icon=":robot:")
+st.title("Chat w/web")
+
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("website URL")
+
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
+else:
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, how can I help you today?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url) 
+
+
+
+    # user input
+    user_query = st.chat_input("Type a message and press enter to send")
+
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+
+        # st.write(response)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+        # test retriever
+        # retrieved_documents = retriever_chain.invoke({
+        #     "chat_history": st.session_state.chat_history,
+        #     "input": user_query
+        # })
+        # st.write(retrieved_documents)
+
+    # display chat history
+    for message in st.session_state.chat_history:
+        if isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
+        elif isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+    
+
+
+# built based on https://www.youtube.com/watch?v=bupx08ZgSFg 
